fc1aeb0c7827ff87e3cfe8b1b39404ae887b6685
yuliangyang <yuliangyang@quliantrip.com>
2015-11-27 14:23:36 +0800

    wifi显示界面

--- Qulian/src/main/java/com/quliantrip/qulian/ui/showFragment/WifiFragment.java
+++ Qulian/src/main/java/com/quliantrip/qulian/ui/showFragment/WifiFragment.java
@@ -1,28 +1,56 @@
 package com.quliantrip.qulian.ui.showFragment;
 
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.quliantrip.qulian.R;
 import com.quliantrip.qulian.base.BaseFragment;
+import com.quliantrip.qulian.scanner.activity.CaptureActivity;
+import com.quliantrip.qulian.scanner.activity.OpenWifiActivity;
 
 /**
  * Created by yuly on 2015/11/9.
  */
 
 
-public class WifiFragment extends BaseFragment {
-
+public class WifiFragment extends BaseFragment implements View.OnClickListener{
+    private Button bt;
     @Override
     public View initView() {
-        TextView textview = new TextView(mContext);
-        textview.setText("wifi");
-        return textview;
+        View view = View.inflate(mContext, R.layout.fragment_main_wifi, null);
+        bt = (Button)view.findViewById(R.id.wifiManage);
+        bt.setOnClickListener(this);
+        return view;
     }
 
     @Override
     public void initDate() {
         System.out.println("wifi数据加载了。。。");
     }
+
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.wifiManage:
+                Intent openCameraIntent = new Intent(mContext,CaptureActivity.class);
+                startActivityForResult(openCameraIntent, 0);
+                break;
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 0) {
+            data.setClass(mContext, OpenWifiActivity.class);
+            startActivity(data);
+
+        }
+    }
+
 
 }
